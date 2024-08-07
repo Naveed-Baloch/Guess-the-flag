d62bb289c42b008ec711b310ddbe287379dd105f
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-07 20:03:27 +0500

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Naveed on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
