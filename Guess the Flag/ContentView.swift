12f45ef72103c535689f787d1efdb4309ec4289e
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-08 21:35:35 +0500

    add assets and guess the flag logic

--- Guess the Flag/ContentView.swift
+++ Guess the Flag/ContentView.swift
@@ -8,27 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isShowingAlert = false
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    
+    @State var correctCountryIndex = Int.random(in: 0...2)
+    @State var guessedCountryIndex = -1
+    @State private var showResult = false
+    @State private var dialogMessage = ""
     var body: some View {
-        VStack{
-            Button {
-                isShowingAlert = true
-            } label: {
-                Label("Edit", systemImage: "pencil")
-                    .padding()
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Text("Guess the Flag")
+                    .textCase(.uppercase)
+                    .font(.title.weight(.bold))
                     .foregroundStyle(.white)
-                    .background(.red)
-                    .containerShape(.capsule)
+     
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.white)
+                        Text(countries[correctCountryIndex])
+                            .font(.largeTitle.weight(.semibold))
+                            .foregroundStyle(.white)
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            guessedCountryIndex = number
+                            checkAnswer()
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.rect(cornerRadius: 10))
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 15))
+                Spacer()
             }
-            .alert("Important message", isPresented: $isShowingAlert) {
-                Button("Delete", role: .destructive) { }
-                Button("Cancel", role: .cancel) { }
-                Button("Edit", role: .destructive) { }
-            } message: {
-                Text("Please read this")
+            .padding(.horizontal, 25)
+        }
+        .alert(dialogMessage,isPresented: $showResult){
+            Button("Next", action: askQuestion)
+            if guessedCountryIndex != correctCountryIndex {
+                Button("Retry", action: { showResult = false })
             }
         }
-        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctCountryIndex = Int.random(in: 0...2)
+    }
+    
+    func checkAnswer() {
+        if guessedCountryIndex == correctCountryIndex {
+            dialogMessage = "Correct Answer Nice Work"
+        } else {
+            dialogMessage = "In correct answer"
+        }
+        showResult = true
     }
 }
 
